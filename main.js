2f5246f8b4b1a3446dd492ebbaa355feb316eeb9
TheMightyRaider <sivasanjay7@gmail.com>
2020-03-29 14:14:37 +0530

    Added remove list from to-do

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-const userInput = document.querySelector("input[name='todo']").value;
+const userInput = document.querySelector("input[name='todo']");
 const todoList = document.querySelector(".todo-list");
 const submit = document.querySelector(".submit");
+let userTask;
 let count = 0;
 
 function displayTask() {
-  const html = `${userInput}  <input type='checkbox' dataset-id=${count++}><br><br>`;
-  todoList.innerHTML += html;
-  //   const checkBox = document.createElement("input");
-  //   checkBox.type = "checkBox";
-  //   checkBox.dataset.id = ++count;
-  //   todoList.insertAdjacentText("beforeend", userInput);
-  //   todoList.appendChild(checkBox);
+  if (userTask) {
+    const html = `<div class='task'> ${userTask}  <input type='checkbox' data-id=${count++}> <button class='remove' data-id=${count++}>Remove</button><br><br> </div>`;
+    todoList.innerHTML += html;
+    document
+      .querySelectorAll(".remove")
+      .forEach(item => item.addEventListener("click", removeTodo));
+    document
+      .querySelectorAll('input[type="checkbox"]')
+      .forEach(item => item.addEventListener("change", checkToggle));
+  }
 }
 
+function removeTodo(event) {
+  console.log(event.target.parentElement.remove());
+}
+
+function checkToggle(event) {
+  console.log(event);
+  console.log(event.target.checked);
+}
+
+userInput.addEventListener("blur", () => {
+  console.log(userInput.value);
+  userTask = userInput.value;
+});
 submit.addEventListener("click", displayTask);
